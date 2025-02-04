36593e11a677d7618112111132eaa6e5bf3f6210
Anirudh-RG <i-Anirudh@roundglass.com>
2025-02-04 04:54:47 +0000

    s3 from cli

--- scripts/aws_s3_script.sh
+++ scripts/aws_s3_script.sh
@@ -1,6 +1,7 @@
 aws s3api create-bucket --bucket data-store-02 --region ap-south-1
 echo "bucket created"
-aws s3api put-bucket-policy --bucket data-store-02 --policy '{
+
+echo '{
   "Version": "2012-10-17",
   "Statement": [
     {
@@ -11,8 +12,9 @@ aws s3api put-bucket-policy --bucket data-store-02 --policy '{
       "Resource": "arn:aws:s3:::data-store-02/*"
     }
   ]
-}'
+}' > bucket-policy.json
 
+aws s3api put-bucket-policy --bucket data-store-02 --policy file://bucket-policy.json
 echo "policy added"
 
 aws s3api put-public-access-block --bucket data-store-02 --public-access-block-configuration '{
@@ -21,13 +23,17 @@ aws s3api put-public-access-block --bucket data-store-02 --public-access-block-c
   "BlockPublicPolicy": false,
   "RestrictPublicBuckets": false
 }'
-echo "acl done"
-aws s3 cp dist/ s3://data-store-02/ --recursive
-aws s3 website s3://data-store-02/ --index-document index.html --error-document index.html
+echo "public access block settings updated"
 
-echo "dist uploaded printing confirming info"
+aws s3api put-bucket-acl --bucket data-store-02 --acl public-read
+echo "bucket ACL updated"
+
+aws s3 cp dist/ s3://data-store-02/ --recursive
+aws s3 website s3://data-store-02 --index-document index.html --error-document index.html
+echo "dist uploaded"
+
+echo "Confirming settings..."
 aws s3api get-bucket-policy --bucket data-store-02
+aws s3api get-bucket-acl --bucket data-store-02
 aws s3api get-public-access-block --bucket data-store-02
 aws s3api get-bucket-website --bucket data-store-02
-
-
